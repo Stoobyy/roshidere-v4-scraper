fd3f546299f55ee3322154b2ae3384f338dae838
Stooby with two y's <97161453+Stoobyy@users.noreply.github.com>
2022-12-28 10:48:46 +0400

    Fianl

--- script.py
+++ script.py
@@ -5,11 +5,12 @@ import sys
 import subprocess
 import time
 
+
 def script():
     with open(f'Roshidere.txt', 'a') as f:
         f.write('% Roshidere Volume 4.5\n')
         f.write("% Scrapped and EPUB'd by Stooby\n\nTranslated by [Glucose Translations](https://glucosetl.files.wordpress.com/)\n\n![cover](Cover.png)")
-        path_to_tesseract = r'C:\Users\amrit\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+        path_to_tesseract = r'C:\Users\amrit\AppData\Local\Programs\Tesseract-OCR\tesseract.exe' #change this to your tesseract path, or just change amrit to your profile name
         for i in range(1,8):
             f.write(f'\n# Chapter {i}\n')
             files = os.listdir(f'./Chapter {i}/')
