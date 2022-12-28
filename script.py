c95d4670593d9127bbc1609c116ee6f597e1bfad
Stooby with two y's <97161453+Stoobyy@users.noreply.github.com>
2022-12-28 09:50:08 +0400

    Yes

--- script.py
+++ script.py
@@ -3,14 +3,19 @@ from pytesseract import pytesseract
 import os
 
 def script():
-    path_to_tesseract = r'C:\Users\amrit\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-    for i in range(1,8):
-        files = os.listdir(f'./Chapter {i}/')
-        pytesseract.tesseract_cmd = path_to_tesseract
-        for j in files:
-            img = Image.open(f'./Chapter {i}/{j}')
-            text = pytesseract.image_to_string(img)
-            with open(f'Roshidere.txt', 'a') as f:
-                f.write(f'\n\n\nCHAPTER {i}:\n\n\n')
+    with open(f'Roshidere.txt', 'a') as f:
+        f.write('% Roshidere Volume 4.5\n')
+        f.write('% By stooboie\n')
+        path_to_tesseract = r'C:\Users\amrit\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+        for i in range(1,8):
+            f.write(f'# Chapter {i}\n')
+            files = os.listdir(f'./Chapter {i}/')
+            pytesseract.tesseract_cmd = path_to_tesseract
+            for j in files:
+                img = Image.open(f'./Chapter {i}/{j}')
+                text = pytesseract.image_to_string(img)
+                if text == '':
+                    f.write(f'[masha best {i}](./Chapter {i}/{j})\n')
                 f.write(text)
+            f.write('Chapter complete.\n')
     print('Scripting complete. Output is Roshidere.txt')
